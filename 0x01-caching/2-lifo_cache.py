6afb047ed53e3d6d9aff19f5473cb0e01b0c24ac
Daniel Azil <danielazil.fse@gmail.com>
2024-06-26 23:44:42 +0000

    Modified put method's condition checks.

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,19 +15,24 @@ class LIFOCache(BaseCaching):
             Construct the object LIFOCache
         """
         super().__init__()
+        self.order = []
 
     def put(self, key, item):
         """
             A method that inserts cached items
         """
-        if key and item:
-            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
-                self.cache_data[key] = item
-            else:
-                self.cache_data[key] = item
-                keys = list(self.cache_data.keys())
-                print("DISCARD: {}".format(keys[0]))
-                del self.cache_data[keys[0]]
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[-1]))
+                del self.cache_data[self.order[-1]]
+                del self.order[-1]
+            if key in self.order:
+                del self.order[self.order.index(key)]
+            self.order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """
