01f714d8071b9bc5f06382b750db6780521d54a6
Daniel Azil <danielazil.fse@gmail.com>
2024-06-27 10:01:20 +0000

    Modified function declaration with type annotations.

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,7 +9,7 @@
 from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
         Return a tuple of size two containing a start index and an
         end index corresponding to the range of indexes to return in
