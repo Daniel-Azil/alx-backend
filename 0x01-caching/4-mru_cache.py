e8419ea0c696f645c390134264fea62e36b20235
Daniel Azil <danielazil.fse@gmail.com>
2024-06-27 00:05:05 +0000

    A module that defines a class MRUCache that inherits from BaseCaching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+    A module that defines a class MRUCache that inherits from BaseCaching
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+        A class MRUCache that inherits from BaseCaching
+    """
+    def __init__(self):
+        """
+            Construct the object MRUCache
+        """
+        super().__init__()
+        self.asd_order = []
+
+    def put(self, key, item):
+        """
+            A method that inserts cached items
+        """
+        if key is None or item is None:
+            pass
+        else:
+            size = len(self.cache_data)
+            if size >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.asd_order[-1]))
+                del self.cache_data[self.asd_order[-1]]
+                del self.asd_order[-1]
+            if key in self.asd_order:
+                del self.asd_order[self.asd_order.index(key)]
+            self.asd_order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            A method that returns value of given key
+        """
+        if key:
+            if key in self.asd_order:
+                del self.asd_order[self.asd_order.index(key)]
+            self.asd_order.append(key)
+            return self.cache_data.get(key, None)
+        return None
