c5d385f48d7b137a3adc4e5dd6d1c7319d15ae16
Daniel Azil <danielazil.fse@gmail.com>
2024-06-27 09:51:51 +0000

    A module that return a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters.

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+    return a tuple of size two containing a start index and an
+    end index corresponding to the range of indexes to return in
+    a list for those particular pagination parameters.
+"""
+
+from typing import Tuple
+
+
+def index_range(page, page_size):
+    """
+        Return a tuple of size two containing a start index and an
+        end index corresponding to the range of indexes to return in
+        a list for those particular pagination parameters.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
